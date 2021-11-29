a4d689f6470119ff7f118c0040581a164ee4eb94
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-11-29 17:49:02 +0300

    mongo setup

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+require("./db");
 
 const app = express();
 
+app.use(cors());
 
-const PORT= process.env.PORT ||5000;
-
-
-app.listen(PORT,()=>{
-    console.log(`SERVER RUN ON ${PORT}`);
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`SERVER RUN ON ${PORT}`);
+});
