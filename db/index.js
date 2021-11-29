3ce28ecd00a5feb9ca3238f3278f08b6730f01b9
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-11-29 18:00:59 +0300

    user Schema

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const DB_URI= process.env.DB_URI
+const DB_URL= process.env.DB_URL
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-mongoose.connect(DB_URI, options).then(() => {
+mongoose.connect(DB_URL, options).then(() => {
   console.log("DB READY TO USE");
 });
