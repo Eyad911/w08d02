61da85433acc4c371b1c314fa6d1b392f2296907
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-11-29 18:43:46 +0300

    done

--- routers/routes/role.js
+++ routers/routes/role.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const {
-    create,
-    getRoles,
-  } = require("./../controllers/role");
-const roleRouter =express.Router();
+const { create, getRoles } = require("./../controllers/role");
+const roleRouter = express.Router();
+
 roleRouter.post("/createRole", create);
-roleRouter.get("/rolse",getRoles)
+roleRouter.get("/role", getRoles);
+
 module.exports = roleRouter;
